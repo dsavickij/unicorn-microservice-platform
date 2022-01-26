7da1c4fc18f2b8bcca1f3e83e0695822ff53ec78
Dmitrij <d.savickij@hotmail.com>
2022-01-26 23:23:43 +0200

    added files to gitignore, interface access modifier fixed

--- core/infrastructure/Unicorn.Core.Infrastructure.Communication.MessageBroker/Queue/QueueMessageDispatcher.cs
+++ core/infrastructure/Unicorn.Core.Infrastructure.Communication.MessageBroker/Queue/QueueMessageDispatcher.cs
@@ -3,7 +3,7 @@ using Unicorn.Core.Infrastructure.Communication.MessageBroker.Messages;
 
 namespace Unicorn.Core.Infrastructure.Communication.MessageBroker;
 
-internal interface IQueueMessageDispatcher
+public interface IQueueMessageDispatcher
 {
     Task SendAsync(string methodFullName, UnicornQueueMessage message);
 }
