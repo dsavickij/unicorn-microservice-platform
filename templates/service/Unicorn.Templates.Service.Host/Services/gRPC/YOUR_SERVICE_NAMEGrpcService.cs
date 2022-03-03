1edb26f4a23787018f4302da0fbb7e060f018aaa
Dmitrij <d.savickij@hotmail.com>
2022-03-03 15:19:30 +0200

    further refinement of Unicorn service template

---
+++ templates/service/Unicorn.Templates.Service.Host/Services/gRPC/YOUR_SERVICE_NAMEGrpcService.cs
@@ -0,0 +1,13 @@
+﻿using Grpc.Core;
+using Unicorn.Templates.Service.SDK.Services.gRPC.Protos;
+
+public class YOUR_SERVICE_NAMEGrpcService : Unicorn.Templates.Service.SDK.Services.gRPC.Protos.YOUR_SERVICE_NAMEGrpcService.YOUR_SERVICE_NAMEGrpcServiceBase
+{
+    public override Task<DecimalSubtractionResponse> SubtractDecimal(DecimalSubtractionRequest request, ServerCallContext context)
+    {
+        var result = request.FirstOperand - request.SecondOperand;
+        var response = new DecimalSubtractionResponse { Result = result };
+
+        return Task.FromResult(response);
+    }
+}
