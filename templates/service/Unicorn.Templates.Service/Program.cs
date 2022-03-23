ebb6480a4d546e6208a293bec9c0f3597987efff
Dmitrij <d.savickij@hotmail.com>
2022-03-23 16:27:37 +0200

    made changes to Unicorn microservice template

--- templates/service/Unicorn.Templates.Service/Program.cs
+++ templates/service/Unicorn.Templates.Service/Program.cs
@@ -1,10 +1,13 @@
 using Unicorn.Core.Infrastructure.HostConfiguration.SDK;
+using Unicorn.Core.Infrastructure.HostConfiguration.SDK.Settings.Defaults;
 using Unicorn.Templates.Service;
 using Unicorn.Templates.Service.Services.gRPC;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // register services on builder.Services if needed
+
+builder.Services.AddHealthChecks(); // add health checks for databases, etc.
 
 builder.Services.AddGrpc();
 
@@ -17,6 +20,8 @@ app.UseUnicornMiddlewares(app.Environment);
 // add middlewares if needed
 
 app.MapGrpcService<YOUR_SERVICE_NAMEGrpcService>();
+
+app.MapHealthChecks(UnicornSettings.HealthCheck.Pattern, UnicornSettings.HealthCheck.Options);
 
 app.MapControllers();
 
