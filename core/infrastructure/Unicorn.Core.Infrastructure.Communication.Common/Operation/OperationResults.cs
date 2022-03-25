82cf2dd2c466e139b590c0722eb050a344fd57c8
Dmitrij <d.savickij@hotmail.com>
2022-03-25 15:38:34 +0200

    Added generic OperationResult.Ok for conveninet usage in grpc clients

--- core/infrastructure/Unicorn.Core.Infrastructure.Communication.Common/Operation/OperationResults.cs
+++ core/infrastructure/Unicorn.Core.Infrastructure.Communication.Common/Operation/OperationResults.cs
@@ -3,6 +3,7 @@
 public abstract class OperationResults
 {
     protected OperationResult Ok() => new(OperationStatusCode.Status200OK);
+    protected OperationResult<TResponse> Ok<TResponse>(TResponse response) => new(OperationStatusCode.Status200OK, response);
     protected OperationResult BadRequest(OperationError error) => BadRequest(new[] { error });
     protected OperationResult BadRequest(IEnumerable<OperationError> errors) => new(OperationStatusCode.Status400BadRequest, errors);
     protected OperationResult Forbidden() => new(OperationStatusCode.Status403Forbidden);
